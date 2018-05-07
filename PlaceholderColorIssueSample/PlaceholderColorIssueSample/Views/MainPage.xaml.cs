a38eeca0cc91b782a4d2b538777723770366c7ff
toussam <mathias.toussaint@outlook.com>
2018-05-07 15:13:36 +0200

    Reproduced the Entry's PlaceholderColor issue

---
+++ PlaceholderColorIssueSample/PlaceholderColorIssueSample/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlaceholderColorIssueSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private SecondPage secondPage;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            secondPage = new SecondPage();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation?.PushAsync(secondPage);
+        }
+    }
+}
---
+++ PlaceholderColorIssueSample/PlaceholderColorIssueSample/Views/SecondPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlaceholderColorIssueSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
